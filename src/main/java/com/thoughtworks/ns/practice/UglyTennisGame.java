7947d6d90ccfbcd0192d7e084b9580e585e1f419
ellyfb <xiefang111222@gmail.com>
2013-11-15 19:47:10 +0800

    modify the tw_ns_practice

--- src/main/java/com/thoughtworks/ns/practice/UglyTennisGame.java
+++ src/main/java/com/thoughtworks/ns/practice/UglyTennisGame.java
@@ -16,86 +16,51 @@ public class UglyTennisGame implements TennisGame {
 
     public String getScore(){
         String score = "";
-        if (P1point == P2point && P1point < 4)
-        {
-            if (P1point==0)
-                score = "Love";
-            if (P1point==1)
-                score = "Fifteen";
-            if (P1point==2)
-                score = "Thirty";
-            score += "-All";
-        }
-        if (P1point==P2point && P1point>=3)
-            score = "Deuce";
 
-        if (P1point > 0 && P2point==0)
-        {
-            if (P1point==1)
-                P1res = "Fifteen";
-            if (P1point==2)
-                P1res = "Thirty";
-            if (P1point==3)
-                P1res = "Forty";
-
-            P2res = "Love";
-            score = P1res + "-" + P2res;
-        }
-        if (P2point > 0 && P1point==0)
-        {
-            if (P2point==1)
-                P2res = "Fifteen";
-            if (P2point==2)
-                P2res = "Thirty";
-            if (P2point==3)
-                P2res = "Forty";
-
+        if(P1point == 0)
             P1res = "Love";
-            score = P1res + "-" + P2res;
-        }
+        else if(P1point == 1)
+            P1res = "Fifteen";
+        else if(P1point == 2)
+            P1res = "Thirty" ;
+        else
+            P1res = "Forty" ;
+        if(P2point == 0)
+            P2res = "Love";
+        else if(P2point == 1)
+            P2res = "Fifteen";
+        else if(P2point == 2)
+            P2res = "Thirty" ;
+        else
+            P2res = "Forty" ;
 
-        if (P1point>P2point && P1point < 4)
-        {
-            if (P1point==2)
-                P1res="Thirty";
-            if (P1point==3)
-                P1res="Forty";
-            if (P2point==1)
-                P2res="Fifteen";
-            if (P2point==2)
-                P2res="Thirty";
-            score = P1res + "-" + P2res;
-        }
-        if (P2point>P1point && P2point < 4)
-        {
-            if (P2point==2)
-                P2res="Thirty";
-            if (P2point==3)
-                P2res="Forty";
-            if (P1point==1)
-                P1res="Fifteen";
-            if (P1point==2)
-                P1res="Thirty";
-            score = P1res + "-" + P2res;
-        }
+         if(P1point == P2point)
+         {
+             if(P1point < 3)
+                  score = P1res + "-" + "All";
+             else
+                  score = "Deuce";
+         }
+         else if(P1point < 4 && P2point < 4)
+             score = P1res + "-" + P2res;
 
         if (P1point > P2point && P2point >= 3)
         {
-            score = "Advantage player1";
+            score = "Advantage "+player1Name;
         }
 
         if (P2point > P1point && P1point >= 3)
         {
-            score = "Advantage player2";
+            score = "Advantage "+player2Name;
         }
 
         if (P1point>=4 && P2point>=0 && (P1point-P2point)>=2)
         {
-            score = "Win for player1";
+            score = "Win for "+player1Name;
         }
         if (P2point>=4 && P1point>=0 && (P2point-P1point)>=2)
         {
-            score = "Win for player2";
+            score = "Win for "+this.player2Name;
         }
         return score;
     }
@@ -104,7 +69,7 @@ public class UglyTennisGame implements TennisGame {
 
         for (int i = 0; i < number; i++)
         {
-            P1Score();
+            P1point++;
         }
 
     }
@@ -113,23 +78,14 @@ public class UglyTennisGame implements TennisGame {
 
         for (int i = 0; i < number; i++)
         {
-            P2Score();
+            P2point++;
         }
 
     }
-
-    public void P1Score(){
-        P1point++;
-    }
-
-    public void P2Score(){
-        P2point++;
-    }
-
     public void wonPoint(String player) {
-        if (player == "player1")
-            P1Score();
+        if (player == player1Name)
+            P1point++;
         else
-            P2Score();
+            P2point++;
     }
 }
